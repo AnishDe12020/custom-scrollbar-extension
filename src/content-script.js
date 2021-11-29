708ef3e189232a53bd59454408eff4d6564e2a3f
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-11-29 21:14:06 +0530

    feat: chrome extension injects css to all tabs which customizes the scrollbar

---
+++ src/content-script.js
@@ -0,0 +1,26 @@
+const css = `
+::-webkit-scrollbar {
+    width: 0.5rem;
+}
+
+::-webkit-scrollbar-thumb {
+    background: #7d7d7d;
+    border-radius: 0.25rem;
+}
+::-webkit-scrollbar-thumb:hover {
+    background: #cacaca;
+}
+`;
+
+const head = document.head || document.getElementsByTagName('head')[0];
+const style = document.createElement('style');
+
+head.appendChild(style);
+
+style.type = 'text/css';
+
+if (style.styleSheet) {
+  style.styleSheet.cssText = css;
+} else { 
+  style.appendChild(document.createTextNode(css));
+}
